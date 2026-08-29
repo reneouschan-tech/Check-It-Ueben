cd55ff267a9ebe264c7a83042d381f1a9c17c5ea
reneouschan-tech <reneouschan@gmail.com>
2026-08-29 12:33:29 +0200

    Restore logo branding

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "checkit-shell-v20260825-7";
+const CACHE_NAME = "checkit-shell-v20260829-1";
 const SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260825-7",
-  "./app.js?v=20260825-7",
+  "./styles.css?v=20260829-1",
+  "./app.js?v=20260829-1",
   "./manifest.webmanifest",
   "./checkit-icon-192.png",
   "./checkit-icon-512.png",
