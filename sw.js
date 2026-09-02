dc5848a913d308e73eadbfac0db0f2fbb05a4d47
reneouschan-tech <reneouschan@gmail.com>
2026-09-02 13:24:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "checkit-shell-v20260829-2";
+const CACHE_NAME = "checkit-shell-v20260902-2";
 const SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260829-2",
-  "./app.js?v=20260829-2",
+  "./styles.css?v=20260902-2",
+  "./app.js?v=20260902-2",
   "./manifest.webmanifest",
   "./checkit-icon-192.png",
   "./checkit-icon-512.png",
